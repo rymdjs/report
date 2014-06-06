d5199faab8cb379aca53e907af7d3215f4ce2296
Johan Brook <johan.jvb@gmail.com>
2014-06-06 14:19:09 +0200

    Comments in spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+
 echo "** Misspelled English words **"
-cat sections/*.tex abstracts/abstract_english.tex | aspell --personal ./wordlist -W 3 -d en list | sort | uniq -u
+cat sections/*.tex abstracts/abstract_english.tex \ # Fetch content
+  | aspell \  # Pipe into aspell
+  --personal ./wordlist \  # Use list of ignored words
+  -W 3   \ # Ignore words with a length <= 3
+  -d en  \ # Use English
+  list   \ # List results to stdout
+  | sort | uniq -u # Sort and remove duplicates
+
 echo "** Misspelled Swedish words in abstract **"
-cat abstracts/abstract_swedish.tex | aspell --personal ./ordlista -d sv list | sort | uniq -u
+cat abstracts/abstract_swedish.tex \
+  | aspell \
+  --personal ./ordlista \ # Use Swedish list of ignored words
+  -d sv \ # Use Swedish
+  list  \
+  | sort | uniq -u
