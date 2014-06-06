bb27912caed24a7c5000fab60a0be34f4e93df4b
Johan Brook <johan.jvb@gmail.com>
2014-06-06 14:36:56 +0200

    Fix spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 echo "** Misspelled English words **"
-cat sections/*.tex abstracts/abstract_english.tex \ # Fetch content
-  | aspell \  # Pipe into aspell
-  --personal ./wordlist \  # Use list of ignored words
-  -W 3   \ # Ignore words with a length <= 3
-  -d en  \ # Use English
-  list   \ # List results to stdout
-  | sort | uniq -u # Sort and remove duplicates
+
+cat sections/*.tex abstracts/abstract_english.tex \
+  | aspell \
+  --personal ./wordlist \
+  -W 3   \
+  -d en  \
+  list   \
+  | sort | uniq -u;
 
 echo "** Misspelled Swedish words in abstract **"
 cat abstracts/abstract_swedish.tex \
   | aspell \
-  --personal ./ordlista \ # Use Swedish list of ignored words
-  -d sv \ # Use Swedish
+  --personal ./ordlista \
+  -d sv \
   list  \
-  | sort | uniq -u
+  | sort | uniq -u;
