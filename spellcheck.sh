4218c978773a4570f8a7af3095f7ab1e85f0b757
Johan Brook <johan.jvb@gmail.com>
2014-06-06 14:05:47 +0200

    Add shell script for spellcheck, based on Aspell

---
+++ spellcheck.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "** Misspelled English words **"
+cat sections/*.tex abstracts/abstract_english.tex | aspell --personal ./wordlist -W 3 -d en list | sort | uniq -u
+echo "** Misspelled Swedish words in abstract **"
+cat abstracts/abstract_swedish.tex | aspell --personal ./ordlista -d sv list | sort | uniq -u
